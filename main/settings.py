1912daccd2ab8da821a18c09a01b9c2bafe8047a
iceface <32415733+winlintun@users.noreply.github.com>
2024-12-02 14:48:43 +0800

    Update taipei time zone in settings.py

--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7%nsn5ycyjt)_!9mac-#dcib(_9&f7y$ilr*hu=_a4#$fabswp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -118,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
